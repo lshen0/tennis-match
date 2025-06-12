c7931883081cd45b586d2d1d8fc0a08bc060e9f6
Lydia Shen <les296@cornell.edu>
2025-06-12 09:46:33 -0400

    Style welcome page
    - Reorder table elements

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -1,6 +1,4 @@
-<?php 
-// plus button --> brings you to player_create 
-?>
+<?php  ?>
 
 <!DOCTYPE html>
 <html>
@@ -19,50 +17,33 @@
                 <th>Team 2</th>
             </tr>
             <tr>
-                <!-- team name -->
+                <!-- Team 1 -->
                 <td> 
                     <label for="team1-name">Team Name:</label>
-                    <input type="text" id="team1-name" name="team1_name" required><br>
+                    <input type="text" id="team1-name" name="team1_name" required><br><br>
+                    <label for="team1-coach-fname">Coach First Name:</label>
+                    <input type="text" id="team1-coach-fname" name="team1_coach_fname" required><br><br>
+                    <label for="team1-coach-lname">Coach Last Name:</label>
+                    <input type="text" id="team1-coach-lname" name="team1_coach_lname" required><br><br>
+                    <label for="team1-coach-email">Coach Email:</label>
+                    <input type="text" id="team1-coach-email" name="team1_coach_email" required><br><br>
+                    <label for="team1-coach-phone">Coach Phone:</label>
+                    <input type="text" id="team1-coach-phone" name="team1_coach_phone" required><br><br>
                 </td>
+                <!-- Team 2 -->
                 <td> 
                     <label for="team2-name">Team Name:</label>
-                    <input type="text" id="team2-name" name="team2_name" required><br>
-                </td>
-            </tr>
-            <tr>
-                <!-- coach first name -->
-                <td> 
-                    <label for="team1-coach-fname">Coach First Name:</label>
-                    <input type="text" id="team1-coach-fname" name="team1_coach_fname" required><br>
-                </td>
-                <td> 
+                    <input type="text" id="team2-name" name="team2_name" required><br><br>
                     <label for="team2-coach-fname">Coach First Name:</label>
-                    <input type="text" id="team2-coach-fname" name="team2_coach_fname" required><br>
-                </td>
-            </tr>
-            <tr>
-                <!-- coach last name -->
-                <td> 
-                    <label for="team1-coach-lname">Coach Last Name:</label>
-                    <input type="text" id="team1-coach-lname" name="team1_coach_lname" required><br>
-                </td>
-                <td> 
+                    <input type="text" id="team2-coach-fname" name="team2_coach_fname" required><br><br>
                     <label for="team2-coach-lname">Coach Last Name:</label>
-                    <input type="text" id="team2-coach-lname" name="team2_coach_lname" required><br>
-                </td>
-            </tr>
-            <tr>
-                <!-- coach email -->
-                <td> 
-                    <label for="team1-coach-email">Coach Email:</label>
-                    <input type="text" id="team1-coach-email" name="team1_coach_email" required><br>
-                </td>
-                <td> 
+                    <input type="text" id="team2-coach-lname" name="team2_coach_lname" required><br><br>
                     <label for="team2-coach-email">Coach Email:</label>
-                    <input type="text" id="team2-coach-email" name="team2_coach_email" required><br>
+                    <input type="text" id="team2-coach-email" name="team2_coach_email" required><br><br>
+                    <label for="team2-coach-email">Coach Phone:</label>
+                    <input type="text" id="team2-coach-phone" name="team2_coach_phone" required><br><br>
                 </td>
             </tr>
-            
         </table>
         <div class="submit-container"> <input type="submit" value="Submit"> </div>
     </form>
