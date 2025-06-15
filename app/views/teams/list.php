a8c72277ae2eee8fc19b9caafe1ed6bca1106ea1
Lydia Shen <lshen286@gmail.com>
2025-06-15 15:10:04 -0400

    Update team list

--- app/views/teams/list.php
+++ app/views/teams/list.php
@@ -1,4 +1,4 @@
-<?php
+<!-- 
 require_once __DIR__ . '/../../models/Team.php';
 require_once __DIR__ . '/../../models/Player.php';
 
@@ -17,16 +17,28 @@ $players_team2 = $playerModel->getPlayersByTeam($team2_id);
 // var_dump($players_team1);
 // var_dump($players_team2);
 
-?>
+ -->
 
 <!DOCTYPE HTML>
 <html> 
-    
+
 <head> 
-    <link rel="stylesheet" href="../../includes/styles.css"> 
+    <link rel="stylesheet" href="../../../includes/styles.css"> 
 </head>
 
 <body> 
-
+    <table>
+        <tr>
+            <!-- <th> <h1 class="title"><?php echo $team1['name']?></h1> </th> -->
+            <!-- <th> <h1 class="title"><?php echo $team2['name']?></h1> </th> -->
+            <th class="title"> Team 1 </th> <!-- todo - delete -->
+            <th class="title"> Team 2 </th> <!-- todo - delete -->
+        </tr>
+        <tr>
+            <td> Coach: <?php echo $team1['coach_fname'] . ' ' . $team1['coach_lname'] . ', ' . $team1['coach_email'] ?></td>
+            <td> Coach: <?php echo $team2['coach_fname'] . ' ' . $team2['coach_lname'] . ', ' . $team2['coach_email'] ?></td>
+        </tr>
+    </table>
 </body>
+
 </html>
