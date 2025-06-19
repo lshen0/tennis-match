26aa5fc88d926b6f34c01ea93e4ebb971af4544c
Lydia Shen <lshen286@gmail.com>
2025-06-18 23:39:28 -0400

    Fix Game model

--- app/models/Game.php
+++ app/models/Game.php
@@ -8,18 +8,18 @@ class Game extends Base {
 	protected $table = 'game';
 
 	/**
-	 * Get set by game id.
+	 * Get game by set id.
 	 */
-	public function getGameBySet($game_id) {
-		return $this->getAllByField('game_id', $game_id, 'i');
+	public function getGameBySet($set_id) {
+		return $this->getAllByField('set_id', $game_id, 'i');
 	}
 
 	/**
 	 * Get games by set id and winner number.
 	 */
-	public function getGamesBySetAndWinner($game_id, $winner) {
-		$sql = "SELECT * FROM $this->table WHERE game_id = ? AND winner = ?";
-		$stmt = $this->execute($sql, [$game_id, $winner], 'ii');
+	public function getGamesBySetAndWinner($set_id, $winner) {
+		$sql = "SELECT * FROM $this->table WHERE set_id = ? AND winner = ?";
+		$stmt = $this->execute($sql, [$set_id, $winner], 'ii');
 		$result = $stmt->get_result();
 		return $result->fetch_all(MYSQLI_ASSOC);
 	}
